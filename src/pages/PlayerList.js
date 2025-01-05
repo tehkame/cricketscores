edf52a03bd31e300d5bade9b315629afec9fbf59
Julian Kruup <treeprosperingapricot@gmail.com>
2025-01-05 15:33:05 +1100

    Added Cell edited event

--- src/pages/PlayerList.js
+++ src/pages/PlayerList.js
@@ -4,15 +4,17 @@ import 'tabulator-tables/dist/css/tabulator_bootstrap4.min.css';
 
 Tabulator.registerModule([EditModule]);
 
+const apiUrl = 'https://csapi-b6cvdxergbf9h5e7.australiasoutheast-01.azurewebsites.net';
+
 const PlayerList = () => {
   const tableRef = useRef(null);
 
   useEffect(() => {
-    fetch('https://csapi-b6cvdxergbf9h5e7.australiasoutheast-01.azurewebsites.net/playerlist')
+    fetch(`${apiUrl}/playerlist`)
       .then((response) => response.json())
       .then((tabledata) => {
         const table = new Tabulator(tableRef.current, {
-          layout:'fitColumns',
+          layout:'fitData',
           data: tabledata,
           columns: [
             { title: 'Name', 
@@ -38,6 +40,29 @@ const PlayerList = () => {
             },
           ],
         });
+        table.on("cellEdited", function(cell){
+          const updatedData = cell.getData();
+          const updatedField = cell.getField();
+          const updatedValue = cell.getValue();
+          fetch(`${apiUrl}/players/${updatedData.Id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              field: updatedField,
+              value: updatedValue
+            })
+          })
+          .then(response => response.json())
+          .then(data => {
+            console.log('Data updated successfully', data);
+          })
+          .catch((error) => {
+            console.error('Error updating data', error);
+          });
+        }
+        );
         return () => table.destroy();
       });
     }, []);  
