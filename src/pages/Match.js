c1fa9030e0788a2b9ff63ac00738b9c5cf38bfef
tehkame <treeprosperingapricot@gmail.com>
2025-01-17 17:26:15 +1100

    terter

--- src/pages/Match.js
+++ src/pages/Match.js
@@ -25,32 +25,41 @@ const Match = () => {
       if(!pageData) return;
 },[pageData]);
 
-    const {Teams} = pageData;
-
   return  <div className="container-fluid bg-light min-vh-100 d-flex flex-column align-items-center pt-4">
     {pageData ? 
     (
             
           <div className="mb-3 w-100">
              <div className="border rounded p-1"><span className="fw-bold d-block w-100">Game Name</span></div>
-            <div className="d-flex justify-content-between">
+             <div className="d-flex justify-content-between align-items-center">
 
-              <div>
-              <div className="border rounded p-1"><span className="fw-bold d-block">${pageData?.teams[0].Name}</span></div>
-              <div className="border rounded p-1"><span className="fw-bold d-block">${Teams[1].Name}</span></div>
-              </div>
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="border rounded p-1">
+        <span className="fw-bold d-block">{pageData.Teams[0].Name}</span>
+      </div>
+      <div className="border rounded p-1">
+        <span className="fw-bold d-block">{pageData.Teams[1].Name}</span>
+      </div>
+    </div>
 
-              <div>
-              <div className="border rounded p-1"><span className="fw-bold d-block">1</span></div>
-              <div className="border rounded p-1"><span className="fw-bold d-block">2</span></div>
-              </div>
+    <div className="d-flex flex-column flex-shrink-0 mx-2">
+      <div className="border rounded p-1">
+        <span className="fw-bold d-block">{pageData.Teams[0].Score1}</span>
+      </div>
+      <div className="border rounded p-1">
+        <span className="fw-bold d-block">{pageData.Teams[1].Score1}</span>
+      </div>
+    </div>
 
-              <div>
-              <div className="border rounded p-1"><span className="fw-bold d-block">3</span></div>
-              <div className="border rounded p-1"><span className="fw-bold d-block">4</span></div>
-              </div>
-
-            </div>
+    <div className="d-flex flex-column flex-shrink-0">
+      <div className="border rounded p-1">
+        <span className="fw-bold d-block">{pageData.Teams[0].Score2}</span>
+      </div>
+      <div className="border rounded p-1">
+        <span className="fw-bold d-block">{pageData.Teams[1].Score2}</span>
+      </div>
+    </div>
+  </div>
           </div>
     ) :
     (
