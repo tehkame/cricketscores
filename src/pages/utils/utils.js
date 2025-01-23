26221ee0343a590be9f154ec264ed6850666bd03
Julian Kruup <treeprosperingapricot@gmail.com>
2025-01-23 13:38:42 +1100

    sdfsdf

---
+++ src/pages/utils/utils.js
@@ -0,0 +1,24 @@
+import { diceImg } from "./constants";
+  
+export function getD6(n) { 
+    return `<img src="data:image/png;base64,${diceImg[n-1]}" style="margin-left: 1px; margin-bottom: 4px;"/>`
+}
+
+export function getRandomTo(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+export function getById(source, id) {
+    const p =  source.filter((b) => b.Id==id)[0];
+    console.log("Found player")
+    console.log(p)
+    return p;
+}
+
+export function updateProperty(setter, property, value) {
+    setter((prev) => ({...prev,  [property]: value }));
+}
+
+export function  handleChangeById(event, source, setter){
+    setter(getById(source, event.target.value));
+}
